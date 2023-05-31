aaa10c4fdefe98ccb8357b2ec9c681d1acf0a850
nabsabraham <nabila.abraham@gmail.com>
2023-05-31 16:00:45 -0400

    changing backend

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 function App() {
   const [text, setText] = useState("");
   const pingServer = async () => {
-    const url = "https://vercel-fastapi-react.vercel.app/";
+    const url = "api/";
     try {
       const response = await fetch(url, {
         method: "GET",
